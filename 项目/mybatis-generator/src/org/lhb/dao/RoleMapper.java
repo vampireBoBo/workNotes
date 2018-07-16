3161b10f47d85ec8792ef86f30100bb2159f1c7a
vampireBOBO <1579404542@qq.com>
2018-07-16 10:59:10 +0800

    新增利用mybatis-generator插件自动生成后台代码

---
+++ 项目/mybatis-generator/src/org/lhb/dao/RoleMapper.java
@@ -0,0 +1,17 @@
+package org.lhb.dao;
+
+import org.lhb.entity.Role;
+
+public interface RoleMapper {
+    int deleteByPrimaryKey(Integer roleId);
+
+    int insert(Role record);
+
+    int insertSelective(Role record);
+
+    Role selectByPrimaryKey(Integer roleId);
+
+    int updateByPrimaryKeySelective(Role record);
+
+    int updateByPrimaryKey(Role record);
+}
---
+++ 项目/mybatis-generator/src/org/lhb/entity/User.java
@@ -0,0 +1,95 @@
+package org.lhb.entity;
+
+import java.util.Date;
+
+public class User {
+    private Integer userId;
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    private String phone;
+
+    private String salt;
+
+    private Boolean status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt == null ? null : salt.trim();
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
